fc55b1f5d07d92745fc79d938306216f4d0bc746
stiles-micaiah <syluxprime40@gmail.com>
2019-07-18 17:15:15 -0600

    Leavng

---
+++ Setup.sql
@@ -0,0 +1,30 @@
+USE somename;
+-- DROP TABLE posts;
+-- DROP TABLE users;
+
+
+
+-- CREATE TABLE users 
+-- (
+-- id INT AUTO_INCREMENT,
+-- name VARCHAR(255) NOT NULL,
+-- passHash VARCHAR(255) NOT NULL,
+-- userName VARCHAR(255) NOT NULL,
+-- email VARCHAR(255) NOT NULL,
+-- phone INT,
+-- PRIMARY KEY(id)
+-- );
+
+-- CREATE TABLE posts 
+-- (
+--     id INT AUTO_INCREMENT,
+--     title VARCHAR(255) NOT NULL,
+--     image VARCHAR(255),
+--     body TEXT NOT NULL,
+--     authorId INT NOT NULL,
+--     timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+--     FOREIGN KEY(authorId)
+--         REFERENCES users(id),
+--     PRIMARY KEY(id)
+-- );
+
